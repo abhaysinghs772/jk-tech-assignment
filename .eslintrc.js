38bf42b98c3ce3a8e1858d25d428422cb2626e31
abhaysinghs772 <abhaysinghs772@gmail.com>
2025-07-13 00:18:57 +0530

    chore: add unix like line break to get rid of LF and CRLF warning in vscode

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,4 +22,5 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
   },
+  "linebreak-style": ["error", "unix"]
 };
