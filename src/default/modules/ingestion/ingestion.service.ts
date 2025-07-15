49249b243412d61959c256b2388195b31d053051
abhaysinghs772 <abhaysinghs772@gmail.com>
2025-07-15 08:16:50 +0530

    wip: ingestion module in progress...

---
+++ src/default/modules/ingestion/ingestion.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class IngestionService {
+    constructor() {}
+
+    async getDocumentById(){
+
+    }
+
+    async createIngestionProcess() {
+
+    }
+
+    async updateIngestionStatus() {
+
+    }
+
+    async findAllIngestionProcesses(){
+
+    }
+
+    async findIngestionProcessById() {
+
+    }
+
+    async findUserIngestionProcesses () {
+
+    }
+
+    async deleteIngestionProcess() {
+
+    }
+}
